f6e20a71fdf2aa9b80d3b682120a960c50ca1df3
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-17 10:27:34 -0500

    create account view and create and migrate table

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,24 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/login' do
+end
+
+post '/logout' do
+end
+
+get '/create' do
+
+  erb :create_account
+end
+
+get '/secret' do
+  if session[:id]
+    erb :secret
+  else  
+   redirect '/login'
+  end
+end
+
+
